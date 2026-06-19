558d5206407676b41b2393f3b159404cf554fe0a
deekshameena8f-python <deekshameena8f@gmail.com>
2026-06-19 06:06:20 +0530

    Implement recursive file discovery scanner

--- File Integrity Monitor/src/scanner.py
+++ File Integrity Monitor/src/scanner.py
@@ -1,1 +1,13 @@
+import os
 
+
+def get_files(directory):
+    files = []
+
+    for root, dirs, filenames in os.walk(directory):
+        for file in filenames:
+            files.append(
+                os.path.join(root, file)
+            )
+
+    return files
