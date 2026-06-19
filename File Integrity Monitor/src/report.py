8d0bdaf1ba13d90f7499749d0fb003c52400cc1c
deekshameena8f-python <deekshameena8f@gmail.com>
2026-06-19 06:25:13 +0530

    Generate integrity monitoring reports

--- File Integrity Monitor/src/report.py
+++ File Integrity Monitor/src/report.py
@@ -1,1 +1,35 @@
+from datetime import datetime
 
+
+def generate_report(
+    modified,
+    new_files,
+    deleted
+):
+
+    report_name = (
+        f"data/reports/"
+        f"report_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+    )
+
+    with open(report_name, "w") as report:
+
+        report.write(
+            "FILE INTEGRITY REPORT\n"
+        )
+
+        report.write(
+            "=" * 40 + "\n\n"
+        )
+
+        report.write(
+            f"Modified Files:\n{modified}\n\n"
+        )
+
+        report.write(
+            f"New Files:\n{new_files}\n\n"
+        )
+
+        report.write(
+            f"Deleted Files:\n{deleted}\n"
+        )
